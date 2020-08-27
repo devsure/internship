55c50cffe5f7c70384af9d32f4b5b7dfe5b335a1
devsure <44438565+devsure@users.noreply.github.com>
2020-08-27 21:22:12 +0530

    Add files via upload

---
+++ Discuss/js/scroll.js
@@ -0,0 +1,5 @@
+function changeColor(){
+  document.querySelector(".f1").style.background-color = "#000";
+  document.querySelector(".f2").style.background-color = "#000";
+  document.querySelector(".f3").style.background-color = "#000";
+}
