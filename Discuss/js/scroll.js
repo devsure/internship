8826b149167d3d577f8a6ef95a9bb0dc08a5f5b1
devsure <44438565+devsure@users.noreply.github.com>
2020-08-27 22:21:54 +0530

    Delete scroll.js

--- Discuss/js/scroll.js
+++
@@ -1,5 +0,0 @@
-function changeColor(){
-  document.querySelector(".f1").style.background-color = "#000";
-  document.querySelector(".f2").style.background-color = "#000";
-  document.querySelector(".f3").style.background-color = "#000";
-}
